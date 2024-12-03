456aec48c3e97c55019000abec50fb51d1f173b4
_m1ster_uzbek_ <jakbarov4401@gmail.com>
2024-12-03 18:12:58 +0500

    Update my.sql

--- my.sql
+++ my.sql
@@ -2,8 +2,11 @@ CREATE DATABASE todo_app;
 
 USE todo_app;
 
-CREATE TABLE todos (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    task VARCHAR(255) NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+create table todo(
+id int primary key auto_increment,
+title varchar(255),
+status enum('pending', 'in_progress', 'completed'),
+due_date datetime,
+created_at datetime,
+updated_ad datetime
 );
